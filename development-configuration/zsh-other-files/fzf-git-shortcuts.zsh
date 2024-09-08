67583577274d1c439dc88f2d293c97f42cb9e34a
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-09-08 11:47:19 +0100

    Add alias to select branch

--- development-configuration/zsh-other-files/fzf-git-shortcuts.zsh
+++ development-configuration/zsh-other-files/fzf-git-shortcuts.zsh
@@ -171,7 +171,7 @@ _fzf_git_files() {
   [[ $root != "$PWD" ]] && query='!../ '
 
   (git -c color.status=$(__fzf_git_color) status --short --no-branch
-   git ls-files "$root" | grep -vxFf <(git status -s | grep '^[^?]' | cut -c4-; echo :) | sed 's/^/   /') |
+    git ls-files "$root" | grep -vxFf <(git status -s | grep '^[^?]' | cut -c4-; echo :) | sed 's/^/   /') |
   _fzf_git_fzf -m --ansi --nth 2..,.. \
     --border-label '📁 Files' \
     --header $'CTRL-O (open in browser) ╱ ALT-E (open in editor)\n\n' \
