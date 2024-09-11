239b95cc390eac905de070cf79898f392bfc6d53
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-09-11 13:27:01 +0100

    Add AWS Timeout Check (#100)

---
+++ development-configuration/scripts/aws_check_timeout.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Fix the format so we can use it
+MODIFIED_DATE="${AWS_SESSION_EXPIRATION/:00/00}"
+
+# ISO 8601 date format
+EXPIRATION_EPOCH=$(date -j -f "%Y-%m-%dT%H:%M:%S%z" "${MODIFIED_DATE}" +"%s")
+
+# Get the current epoch time
+CURRENT_EPOCH=$(date +%s)
+
+# Check if the current epoch is greater than the expiration epoch
+COMPARISON=$((CURRENT_EPOCH > EXPIRATION_EPOCH))
+
+# If the comparison is true, then the session is expired
+if [ $COMPARISON -eq 1 ]; then
+  echo "AWS session has expired."
+  exit 1
+fi
+
+# Calculate the difference between the current epoch and the expiration epoch
+DIFFERENCE=$((EXPIRATION_EPOCH - CURRENT_EPOCH))
+# If the difference is less than 1 minutes, then the session should be refreshed first
+if [ $DIFFERENCE -lt 60 ]; then
+  echo "AWS session has less than 1 minute remaining. Refresh Session."
+  exit 2
+fi
