5ad60f85894a51b3adbb6ac9aa1c4c1f4cab41aa
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-09-11 13:24:36 +0100

    update

--- development-configuration/scripts/aws_check_timeout.sh
+++ development-configuration/scripts/aws_check_timeout.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Fix the format so we can use it
-MODIFIED_DATE=$(echo "${AWS_SESSION_EXPIRATION}" | sed 's/:00/00/')
+MODIFIED_DATE=$(echo "${AWS_SESSION_EXPIRATION/:00/00}")
 
 # ISO 8601 date format
 EXPIRATION_EPOCH=$(date -j -f "%Y-%m-%dT%H:%M:%S%z" "${MODIFIED_DATE}" +"%s")
