158eb1c94ee5fccb08db7fa8738cb5924bdf0385
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-11-04 15:23:37 +0000

    Swap Starship with Oh My Posh (#132)
    # Pull Request
    
    ## Description
    
    This pull request introduces a significant change in the terminal prompt
    configuration by replacing Starship with Oh My Posh. It also includes
    updates to the configuration files and minor formatting adjustments.
    
    ### Terminal Prompt Configuration:
    *
    [`.zshrc`](diffhunk://#diff-4c2d312ff50ee6b26c2cb601fc96a95eceabe4b456831762e5d6caf41b900383L51-R57):
    Replaced Starship initialization with Oh My Posh initialization.
    *
    [`development-configuration/config/oh-my-posh.json`](diffhunk://#diff-406ca8a65cc44725562b891dc02ddb251f3bc1f799a1a2b027ea1f695fe3a2a5R1-R183):
    Added a new configuration file for Oh My Posh.
    
    ### Code Cleanup:
    *
    [`development-configuration/zsh-other-files/graphite-completions.zsh`](diffhunk://#diff-358a4c5827ba18950ccd5875c4820d0937f576f0b7753762eb5ea4e625dbff33L9-R9):
    Minor formatting changes to function definitions.
    [[1]](diffhunk://#diff-358a4c5827ba18950ccd5875c4820d0937f576f0b7753762eb5ea4e625dbff33L9-R9)
    [[2]](diffhunk://#diff-358a4c5827ba18950ccd5875c4820d0937f576f0b7753762eb5ea4e625dbff33L20)
    *
    [`development-configuration/zsh-other-files/starship.zsh`](diffhunk://#diff-8e13648da08bd4b9ef92df6e081f90ed0a75d105daefe4d97faf873e026e6288L1-L92):
    Removed the entire Starship configuration script.

--- development-configuration/zsh-other-files/graphite-completions.zsh
+++ development-configuration/zsh-other-files/graphite-completions.zsh
@@ -6,15 +6,13 @@
 # Installation: gt completion >> ~/.zshrc
 #    or gt completion >> ~/.zprofile on OSX.
 #
-_gt_yargs_completions()
-{
+_gt_yargs_completions() {
   local reply
   local si=$IFS
   IFS=$'
-' reply=($(COMP_CWORD="$((CURRENT-1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" gt --get-yargs-completions "${words[@]}"))
+' reply=($(COMP_CWORD="$((CURRENT - 1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" gt --get-yargs-completions "${words[@]}"))
   IFS=$si
   _describe 'values' reply
 }
 compdef _gt_yargs_completions gt
 ###-end-gt-completions-###
-
