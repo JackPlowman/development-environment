48aba4a5a0e31c7fa21217f0dcd6651790c5f5b7
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-05-16 11:21:59 +0100

    Add Update No Verify Script (#254)
    # Pull Request
    
    ## Description
    
    This pull request introduces a new alias for skipping pre-commit
    verification during updates, fixes a typo in commit messages across
    scripts, and adds a new script for performing updates without
    verification. Below is a summary of the most important changes:
    
    ### New functionality:
    * Added a new alias `update_no_verify` in `.zshrc` to run the
    `update_no_verify_and_push.sh` script, which skips pre-commit hooks
    during updates. (`.zshrc`,
    [.zshrcR102](diffhunk://#diff-4c2d312ff50ee6b26c2cb601fc96a95eceabe4b456831762e5d6caf41b900383R102))
    * Introduced a new script `update_no_verify_and_push.sh` that commits
    changes without running pre-commit hooks and pushes them to the remote
    repository.
    (`development-configuration/scripts/update_no_verify_and_push.sh`,
    [development-configuration/scripts/update_no_verify_and_push.shR1-R9](diffhunk://#diff-e7320f577b28173b09a6a442ac529c02cb44ad8142810be8fd508937ac2a4c2bR1-R9))
    
    ### Bug fixes:
    * Fixed a typo in the echo message ("Commiting" -> "Committing") in
    `commit_and_push.sh`.
    (`development-configuration/scripts/commit_and_push.sh`,
    [development-configuration/scripts/commit_and_push.shL4-R4](diffhunk://#diff-f3aa3dd3007c91c993e0c1ed2681691e65bd4a15fcae9b83876f29be858b2304L4-R4))
    * Fixed the same typo in `update_and_push.sh`.
    (`development-configuration/scripts/update_and_push.sh`,
    [development-configuration/scripts/update_and_push.shL4-R4](diffhunk://#diff-6f20a8bd31ecbf8f92fc87ca763c8fdd2a7f1d235fd6fd9abb7b7a113ba72d7aL4-R4))

--- development-configuration/scripts/commit_and_push.sh
+++ development-configuration/scripts/commit_and_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Commit the changes with the provided commit title
-echo "Commiting staged changes..."
+echo "Committing staged changes..."
 git commit -m "$1"
 
 # # Push the changes to the remote repository
--- development-configuration/scripts/update_no_verify_and_push.sh
+++ development-configuration/scripts/update_no_verify_and_push.sh
@@ -2,7 +2,7 @@
 
 # Commit the changes
 echo "Committing staged changes..."
-git commit -m "update"
+git commit -m "update" --no-verify
 
 # # Push the changes to the remote repository
 echo "Pushing changes to the remote repository..."
